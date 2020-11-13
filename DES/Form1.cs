b6cabdb3f7d83890a9a512bd0921530369cb13ea
vladimirtonkov <62721503+vladimirtonkov@users.noreply.github.com>
2020-11-14 00:31:42 +0400

    Update Form1.cs

--- DES/Form1.cs
+++ DES/Form1.cs
@@ -24,18 +24,17 @@ namespace DES
         {
             sKey = textBox1.Text;
 
-            //условие выбора файла которое необходимо будет зашифровать
             if(openFileDialog1.ShowDialog() == DialogResult.OK)
             {
                 string source = openFileDialog1.FileName;
-                saveFileDialog1.Filter = "des files |*.des";   //формат сохранения
+                saveFileDialog1.Filter = "des files |*.des";   
 
-                // место выбора где будет сохранен файл
+             
                 if(saveFileDialog1.ShowDialog() == DialogResult.OK)
                 {
-                    // переменная куда будет записываться зашифрованный текст из файла
+                   
                     string destination = saveFileDialog1.FileName;
-                    //само шифрование
+                    
                     EncryptFile(source, destination, sKey);
 
                 }
@@ -51,24 +50,23 @@ namespace DES
             try
             {
                 DES.Key = ASCIIEncoding.ASCII.GetBytes(sKey);
-                // вектор инициализации
+                
                 DES.IV = ASCIIEncoding.ASCII.GetBytes(sKey);
 
-                // ICryptoTransform базовые операции криптографических преобразование
-                // CreateDecryptor создает симметрические объекты шифровования с тек. ключами 
+                
                 ICryptoTransform desencrypt = DES.CreateEncryptor();
                 CryptoStream cryptoStream = new CryptoStream(fsEncrypt, desencrypt, CryptoStreamMode.Write);
                 byte[] bytearrayinput = new byte[fsInput.Length - 0];
 
-                //   fsInput производит чтение блока байт 
+              
                 fsInput.Read(bytearrayinput, 0, bytearrayinput.Length);
 
-                // cryptoStream записывает последовательность байтов в текущий cryptoStream
+                
                 cryptoStream.Write(bytearrayinput, 0, bytearrayinput.Length);
                 cryptoStream.Close();
 
                }
-            // если ключ будет больше или меньше 8 символов или ключ будет не верный 
+            
             catch
             {
                 MessageBox.Show("ошибка");
@@ -86,24 +84,22 @@ namespace DES
             try
             {
                 DES.Key = ASCIIEncoding.ASCII.GetBytes(sKey);
-                // вектор инициализации
+               
                 DES.IV = ASCIIEncoding.ASCII.GetBytes(sKey);
 
-                // ICryptoTransform базовые операции криптографических преобразование
-                // CreateDecryptor создает симметрические объекты шифровования с тек. ключами 
+               
                 ICryptoTransform desencrypt = DES.CreateDecryptor();
                 CryptoStream cryptoStream = new CryptoStream(fsEncrypt, desencrypt, CryptoStreamMode.Write);
                 byte[] bytearrayinput = new byte[fsInput.Length - 0];
-
-                //   fsInput производит чтение блока байт 
+ 
                 fsInput.Read(bytearrayinput, 0, bytearrayinput.Length);
 
-                // cryptoStream записывает последовательность байтов в текущий cryptoStream
+              
                 cryptoStream.Write(bytearrayinput, 0, bytearrayinput.Length);
                 cryptoStream.Close();
 
             }
-            // если ключ будет больше или меньше 8 символов или ключ будет не верный 
+        
             catch
             {
                 MessageBox.Show("ошибка");
@@ -117,20 +113,19 @@ namespace DES
         {
             sKey = textBox1.Text;
 
-            //позволяет находить файл с разрешением
             openFileDialog1.Filter = "des files |*.des";
-            //условие выбора файла которое необходимо будет зашифровать
+          
             if (openFileDialog1.ShowDialog() == DialogResult.OK)
             {
                 string source = openFileDialog1.FileName;
-                saveFileDialog1.Filter = "txt files |*.txt";   //формат сохранения
+                saveFileDialog1.Filter = "txt files |*.txt";   
 
-                // место выбора где будет сохранен файл
+              
                 if (saveFileDialog1.ShowDialog() == DialogResult.OK)
                 {
-                    // переменная куда будет записываться зашифрованный текст из файла
+                    
                     string destination = saveFileDialog1.FileName;
-                    //само шифрование
+                    
                     DecryptFile(source, destination, sKey);
 
                 }
